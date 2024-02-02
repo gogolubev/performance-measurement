e2adf1f74a946261f0b002e3809fbd5a2df01935
Golubev Alex <golubevuav@gmail.com>
2024-02-02 21:04:47 +0100

    chore: install navigation. set up babel resolve pluging. set up alias

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,14 @@
 module.exports = {
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.jsx', '.js', '.ts', '.tsx', '.json'],
+        alias: {
+          '@': './app',
+        },
+      },
+    ],
+  ],
   presets: ['module:@react-native/babel-preset'],
 };
